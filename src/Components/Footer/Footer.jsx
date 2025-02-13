cbd58e210eb34c68253e6576b411f4e1dfa545fa
Kanishka-Che <akcadikari@std.appsc.sab.ac.lk>
2025-02-13 12:17:23 +0530

    modify Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+      <footer className="footer">
+        <p>&copy; 2025 Urban Vogue. All rights reserved.</p>
+        <p>Designed with ❤️ by Urban Vogue Team</p>
+      </footer>
+  );
+};
+
+export default Footer; 
